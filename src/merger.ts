80c8875123616d74cfaf315dee9fd99e17cd51a4
kimu_shu <alfvegardrisc@gmail.com>
2017-03-26 13:59:15 +0900

    Implement updater

---
+++ src/merger.ts
@@ -0,0 +1,110 @@
+'use strict';
+
+import deepEqual = require('deep-equal');
+
+let hierarchy: string[] = [];
+
+function printLog(msg: string, key: string, val?: string): void {
+    let msg2 = (msg + "         ").substr(0, 9);
+    let path = hierarchy.concat(key).join(".").replace(/\.\[/g, "[");
+    if (val && val.length > 80) {
+        val = val.substr(0, 80) + "...";
+    }
+    console.log(`[${msg2}] ${path}: ${val}`);
+}
+
+export function mergeObject<T>(rules: Object, ref: T, old: T): Promise<T> {
+    let ret: T = <T>{};
+    if (old == null) { old = <T>{}; }
+    let keys = Object.keys(ref);
+    Object.keys(rules).forEach((key) => {
+        if (keys.indexOf(key) < 0) { keys.push(key); }
+    })
+    Object.keys(old).forEach((key) => {
+        if (key.startsWith("_")) { return; }
+        if (keys.indexOf(key) < 0) {
+            printLog("REMOVED", key, JSON.stringify(old[key]))
+        }
+    })
+    return keys.reduce(
+        (promise, key) => { return promise.then(() => {
+            if (key.startsWith("_")) { return; }
+            let rval = ref[key];
+            let oval = old[key];
+            if (Object.prototype.hasOwnProperty.call(rules, key)) {
+                let rule = rules[<any>key];
+                if (rule == null) {
+                    // Drop this key
+                    printLog("DROPPED", key);
+                    return;
+                } else if (typeof(rule) !== "function") {
+                    // Overwrite value
+                    rval = rule;
+                } else if (rule.length === 1) {
+                    // Convert values
+                    rval = rule(rval);
+                    oval = rule(oval);
+                } else {
+                    // Transfer another merge function
+                    hierarchy.push(key);
+                    return rule(rval, oval, ref, old).then((val) => {
+                        hierarchy.pop();
+                        if (val != null) {
+                            ret[key] = val;
+                        } else {
+                            printLog("DROPPED", key);
+                        }
+                    });
+                }
+            }
+            // Simple copy
+            if (oval == null) {
+                printLog("NEW_VALUE", key, JSON.stringify(rval));
+            } else if (!deepEqual(oval, rval, {strict: true})) {
+                printLog("UPDATE", key, JSON.stringify(rval));
+            }
+            ret[key] = rval;
+        }); }, Promise.resolve()
+    ).then(() => {
+        return ret;
+    });
+}
+
+export function mergeObjectArray<T>(identifier: (item: T) => any, rules: Object, ref: T[], old: T[]): Promise<T[]> {
+    let ret: T[] = [];
+    let used: boolean[] = [];
+    if (old == null) { old = []; }
+    return ref.reduce(
+        (promise, rval, i) => { return promise.then(() => {
+            let oval: T;
+            let rid = identifier(rval);
+            if (rid == null) {
+                // Drop this item
+                printLog("DROPPED", `[${i}]`);
+                return;
+            }
+            let found = old.findIndex((oval) => identifier(oval) == rid);
+            if (found < 0) {
+                printLog("NEW_ITEM", `[${i}]`, `(${rid})`);
+            } else {
+                used[found] = true;
+                oval = old[found];
+            }
+            hierarchy.push(`[${i}]`);
+            return mergeObject(rules, rval, oval).then((val) => {
+                ret.push(val);
+                hierarchy.pop();
+            });
+        }); }, Promise.resolve()
+    ).then(() => {
+        for (let i = 0; i < old.length; ++i) {
+            if (!used[i]) {
+                let oid = identifier(old[i]);
+                if (oid != null) {
+                    printLog("REMOVED", "[]", `(${oid})`);
+                }
+            }
+        }
+        return ret;
+    });
+}
--- src/update.ts
+++ src/update.ts
@@ -1,18 +1,181 @@
 'use strict';
 
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
+import { mergeObject, mergeObjectArray } from './merger'
+import * as util from 'util';
+import uuidV4 = require('uuid/v4');
+import GitHub = require('github');
 
-let current: RubicCatalog.Root;
-let template: RubicCatalog.Root;
+const AUTH_JSON     = "auth.json"
+const CATALOG_JSON  = "catalog.json";
+const TEMPLATE_JSON = "template.json";
+const FIRMWARE_JSON = "firmware.json";
+const RELEASE_JSON  = "release.json";
+const ASSET_PATTERN = /\.(zip|tar\.gz|tgz)$/i;
+
+let github = new GitHub({
+    protocol: "https",
+    headers: {
+        "user-agent": "rubic-catalog/update"
+    },
+    Promise: Promise
+});
+function toDateNowNumber(from: any): number {
+    if (typeof(from) == "number") { return from; }
+    return Date.parse(from);
+}
+
+let ghlimit = {limit: 0, remaining: 0, reset: 0};
+function monitorGH(promise: Promise<any>): Promise<any> {
+    return promise.then((data) => {
+        let meta = (data && data.meta);
+        if (meta) {
+            ghlimit.limit = parseInt(meta["x-ratelimit-limit"]);
+            ghlimit.remaining = parseInt(meta["x-ratelimit-remaining"]);
+            ghlimit.reset = parseInt(meta["x-ratelimit-reset"]);
+        }
+        return data;
+    });
+}
+
+function reportGH(): void {
+    console.info(`GitHub API RateLimit: ${ghlimit.remaining} / ${ghlimit.limit}`);
+    console.info(`GitHub API RateLimit Reset: ${new Date(ghlimit.reset * 1000).toString()}`)
+}
+
+let old: RubicCatalog.Root;
+let ref: RubicCatalog.Root;
+
+// Read GitHub authentication
+try {
+    let auth = JSON.parse(readFileSync(AUTH_JSON, "utf8"));
+    github.authenticate(auth);
+} catch (error) {
+    console.warn("GitHub authentication is not enabled. API limit may be too small!");
+}
 
 // Read current catalog.json content
 try {
-    current = <RubicCatalog.Root>JSON.parse(readFileSync("catalog.json", "utf8"));
+    old = <RubicCatalog.Root>JSON.parse(readFileSync(CATALOG_JSON, "utf8"));
 } catch (error) {
     // Ignore errors
 }
 
 // Read template
-template = <RubicCatalog.Root>JSON.parse(readFileSync("catalog.template.json", "utf8"));
+ref = <RubicCatalog.Root>JSON.parse(readFileSync(TEMPLATE_JSON, "utf8"));
 
+// GitHub access parameters
+let ghopt: any = {};
 
+// Merge rule definitions
+let relSummaryMergeRules = {
+    published_at: toDateNowNumber,
+    updated_at: toDateNowNumber
+};
+let firmDetailMergeRules = {
+    releases: (refArray: RubicCatalog.ReleaseSummary[], oldArray: RubicCatalog.ReleaseSummary[]) => {
+        let promise: Promise<void>;
+        if (!refArray) {
+            promise = Promise.resolve({
+            }).then(() => {
+                return monitorGH((<any>github.repos).getReleases(ghopt));
+            }).then((releases: any) => {
+                refArray = [];
+                releases.data.forEach((rel) => {
+                    if (rel.draft) { return; }  // Do not include draft releases
+                    let url;
+                    let updated_at;
+                    (rel.assets || []).forEach((asset) => {
+                        if (!url && ASSET_PATTERN.test(asset.name)) {
+                            url = asset.browser_download_url;
+                            updated_at = asset.updated_at;
+                        }
+                    });
+                    refArray.push({
+                        tag: rel.tag_name,
+                        name: rel.name,
+                        description: rel.body,
+                        published_at: rel.published_at,
+                        updated_at: updated_at,
+                        author: rel.author.login,
+                        url: url,
+                        cache: null
+                    });
+                });
+            });
+        } else {
+            promise = Promise.resolve();
+        }
+        return promise.then(() => {
+            return mergeObjectArray(
+                (relSummary: RubicCatalog.ReleaseSummary) => relSummary.tag,
+                relSummaryMergeRules,
+                refArray, oldArray
+            );
+        });
+    }
+};
+let firmSummaryMergeRules = {
+    cache: (ignored: null, old: RubicCatalog.FirmwareDetail, refSummary: RubicCatalog.FirmwareSummary) => {
+        if (refSummary.host !== "github") {
+            return Promise.reject(Error(`Unknown service host: ${refSummary.host}`));
+        }
+        if (refSummary.disabled) {
+            return Promise.resolve();
+        }
+        ghopt = {owner: refSummary.owner, repo: refSummary.repo};
+        console.info(`Receiving catalog from GitHub (${ghopt.owner}/${ghopt.repo})`)
+        return Promise.resolve({
+        }).then(() => {
+            return monitorGH(github.gitdata.getReference(Object.assign({
+                ref: `heads/${refSummary.branch || "master"}`
+            }, ghopt)));
+        }).then((reference: any) => {
+            return monitorGH(github.gitdata.getCommit(Object.assign({
+                sha: reference.data.object.sha
+            }, ghopt)));
+        }).then((commit: any) => {
+            return monitorGH(github.gitdata.getTree(Object.assign({
+                sha: commit.data.tree.sha
+            }, ghopt)));
+        }).then((tree: any) => {
+            let file = tree.data.tree.find((item) => item.path === "firmware.json");
+            if (!file) {
+                return Promise.reject(
+                    Error("This repository does not have firmware.json")
+                );
+            }
+            return monitorGH(github.gitdata.getBlob(Object.assign({
+                sha: file.sha
+            }, ghopt)));
+        }).then((blob: any) => {
+            let {content, encoding} = blob.data;
+            let jsonText = Buffer.from(content, encoding).toString();
+            return JSON.parse(jsonText);
+        }).then((ref: RubicCatalog.FirmwareDetail) => {
+            return mergeObject(firmDetailMergeRules, ref, old);
+        });
+    }
+};
+let boardMergeRules = {
+    firmwares: mergeObjectArray.bind(null, (firmSummary: RubicCatalog.FirmwareSummary) => {
+        return firmSummary.uuid || (uuidV4());
+    }, firmSummaryMergeRules)
+};
+let rootMergeRules = {
+    lastModified: Date.now(),
+    boards: mergeObjectArray.bind(null, (board: RubicCatalog.Board) => {
+        return board.class;
+    }, boardMergeRules)
+};
+
+// Merge
+mergeObject(rootMergeRules, ref, old).then((newObj) => {
+    reportGH();
+    writeFileSync(CATALOG_JSON, JSON.stringify(newObj, null, "  "));
+    process.exit(0);
+}).catch((error) => {
+    reportGH();
+    console.dir(error);
+    process.exit(1);
+});
